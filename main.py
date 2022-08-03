07a1297459c25a7756217d3de0c7fc3c68991b21
manish-24 <57222580+manish-24@users.noreply.github.com>
2022-08-03 10:31:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
+import json
+import sqlite3
 import csv
 import requests
-import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-out = open("output.json", "w")
 
+conn = sqlite3.connect("scrapped.s3db")
+cur = conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS scrap(id INTEGER primary key, title TEXT, uri TEXT, price TEXT,detail TEXT)")
+conn.commit()
 arr = []
 
 
-
-
-def func(url):
-    map = {}
+def scrap_data(i, uri):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(url)
+    driver.get(uri)
     title = driver.find_element("id", "productTitle").text
-    map["title"]=title
-    print(title)
     image = driver.find_element("id", "imgBlkFront").get_attribute("src")
-    map['image']=image
-    print(image)
     price = driver.find_element("xpath", '//*[@id="a-autoid-3-announce"]/span[2]/span').text
-    map["price"]=price
-    print(price)
     product_detail = driver.find_element("id", "detailBullets_feature_div").text
-    map["product_detail"] = product_detail
-    print(product_detail)
-    arr.append(map)
+    arr.append({"title": title, "image_uri": image, "Price": price, "Product Details": product_detail})
+    cur.execute(f"INSERT INTO scrap (id, title, uri, price, detail) VALUES (?, ?, ?, ?, ?);",
+                (i, title, image, price, product_detail))
     driver.close()
-# func("https://www.amazon.it/dp/000416198X")
+
 with open('amazon.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    for line_count, row in enumerate(csv_reader):
-        if line_count > 0:
-            url = f'https://www.amazon.{row[3]}/dp/{row[2]}'
-            response = requests.get(url)
-            if response.status_code == 200:
-                func(url)
-            else:
-                print(f"{url} Not Available")
-json.dump(json.dumps(arr), out)
-out.close()
+for line_count, row in enumerate(csv_reader):
+    if line_count > 0:
+        url = f'https://www.amazon.{row[3]}/dp/{row[2]}'
+        response_code = requests.get(url).status_code
+        if response_code == 200:
+            scrap_data(line_count, url)
+        else:
+            print(f"{url} Not Available")
+conn.commit()
+with open("output.json", "w") as out:
+    json.dump(arr, out)
